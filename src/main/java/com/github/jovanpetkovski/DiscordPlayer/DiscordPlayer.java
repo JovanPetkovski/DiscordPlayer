23ef12d0dc84bb635622e24916a2c52c54a45472
JovanPetkovski <90635233+JovanPetkovski@users.noreply.github.com>
2021-09-14 00:41:52 +0200

    Discord Player Bot

--- src/main/java/com/github/jovanpetkovski/DiscordPlayer/DiscordPlayer.java
+++ src/main/java/com/github/jovanpetkovski/DiscordPlayer/DiscordPlayer.java
@@ -1,15 +1,9 @@
 package com.github.jovanpetkovski.DiscordPlayer;
 
-import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats;
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
@@ -19,8 +13,6 @@ import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.channel.VoiceChannel;
 import discord4j.voice.AudioProvider;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -29,78 +21,9 @@ import java.util.Map;
 interface Command {
     void execute(MessageCreateEvent event);
 }
-
-final class TrackScheduler implements AudioLoadResultHandler {
-
-    private final AudioPlayer player;
-
-    public TrackScheduler(final AudioPlayer player) {
-        this.player = player;
-    }
-
-    @Override
-    public void trackLoaded(final AudioTrack track) {
-        // LavaPlayer found an audio source for us to play
-        player.playTrack(track);
-    }
-
-    @Override
-    public void playlistLoaded(final AudioPlaylist playlist) {
-        // LavaPlayer found multiple AudioTracks from some playlist
-    }
-
-    @Override
-    public void noMatches() {
-        // LavaPlayer did not find any audio to extract
-    }
-
-    @Override
-    public void loadFailed(final FriendlyException exception) {
-        // LavaPlayer could not parse an audio source for some reason
-    }
-}
-
-final class LavaPlayerAudioProvider extends AudioProvider {
-
-    private final AudioPlayer player;
-    private final MutableAudioFrame frame = new MutableAudioFrame();
-
-    public LavaPlayerAudioProvider(final AudioPlayer player) {
-        // Allocate a ByteBuffer for Discord4J's AudioProvider to hold audio data
-        // for Discord
-        super(
-                ByteBuffer.allocate(
-                        StandardAudioDataFormats.DISCORD_OPUS.maximumChunkSize()
-                )
-        );
-        // Set LavaPlayer's MutableAudioFrame to use the same buffer as the one we
-        // just allocated
-        frame.setBuffer(getBuffer());
-        this.player = player;
-    }
-
-    @Override
-    public boolean provide() {
-        // AudioPlayer writes audio data to its AudioFrame
-        final boolean didProvide = player.provide(frame);
-        // If audio was provided, flip from write-mode to read-mode
-        if (didProvide) {
-            getBuffer().flip();
-        }
-        return didProvide;
-    }
-}
-
 public class DiscordPlayer {
     private static final Map<String, Command> commands = new HashMap<>();
-
-    static {
-        commands.put("ping", event -> event.getMessage()
-                .getChannel().block()
-                .createMessage("Pong!").block());
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
 
         playerManager.getConfiguration()
@@ -118,8 +41,6 @@ public class DiscordPlayer {
                 if (voiceState != null) {
                     final VoiceChannel channel = voiceState.getChannel().block();
                     if (channel != null) {
-                        // join returns a VoiceConnection which would be required if we were
-                        // adding disconnection features, but for now we are just ignoring it.
                         channel.join(spec -> spec.setProvider(provider)).block();
 
                     }
@@ -134,22 +55,36 @@ public class DiscordPlayer {
             final List<String> command = Arrays.asList(content.split(" "));
             playerManager.loadItem(command.get(1), scheduler);
         });
+        commands.put("leave", event -> {
+            final Member member = event.getMember().orElse(null);
+            if (member != null) {
+                final VoiceState voiceState = member.getVoiceState().block();
+                if (voiceState != null) {
+                    final VoiceChannel channel = voiceState.getChannel().block();
+                    if (channel != null) {
+                        channel.sendDisconnectVoiceState().block();
+                    }
+                }
+            }
+            player.destroy();
 
-
+        });
+        commands.put("pause", event -> {
+            player.setPaused(true);
+        });
+        commands.put("unpause", event -> {
+            player.setPaused(false);
+        });
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build()
                 .login()
                 .block();
+        assert client != null;
         client.getEventDispatcher().on(MessageCreateEvent.class)
-                // subscribe is like block, in that it will *request* for action
-                // to be done, but instead of blocking the thread, waiting for it
-                // to finish, it will just execute the results asynchronously.
                 .subscribe(event -> {
-                    // 3.1 Message.getContent() is a String
                     final String content = event.getMessage().getContent();
 
                     for (final Map.Entry<String, Command> entry : commands.entrySet()) {
-                        // We will be using ! as our "prefix" to any command in the system.
-                        if (content.startsWith('!' + entry.getKey())) {
+                        if (content.startsWith('?' + entry.getKey())) {
                             entry.getValue().execute(event);
                             break;
                         }
@@ -157,7 +92,6 @@ public class DiscordPlayer {
                 });
 
         client.onDisconnect().block();
-
     }
 
 }
